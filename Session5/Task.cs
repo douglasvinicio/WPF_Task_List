ed3f39222061e1a654bea032fa5e413d63d0bfd5
douglasvinicio <douglas.vinicio@hotmail.com>
2021-01-13 12:06:25 -0800

    To Do List Final

---
+++ Session5/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session5
+{
+    class Task
+    {
+        private string _taskDescription;
+        private string _difficulty;
+        string _dueDate;
+        string _status;
+
+        public string TaskDescription { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string DueDate{ get; set; }
+
+        public string Status { get; set; }
+        
+        public Task(string tskDesc, string difficulty, string dueDate, string status)
+        {
+            TaskDescription = tskDesc;
+            Difficulty = difficulty;
+            DueDate = dueDate;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return $"{TaskDescription} - {Difficulty} - {DueDate} - {Status}";
+        }
+
+        public string ToDataString()
+        {
+            return $"{TaskDescription};{Difficulty};{DueDate};{Status}";
+        }
+
+    }
+}
